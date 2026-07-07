ea106ab5640c3718e5d66a841c7fbc84eeb40b6e
dhruvgujjar773-max <dhruvgujjar773@gmail.com>
2026-07-08 01:56:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log('loaded');
+window.addEventListener("scroll",()=>{
+
+const cards=document.querySelectorAll(".card,.portfolio-card,.price-card,.stat");
+
+cards.forEach(card=>{
+
+const top=card.getBoundingClientRect().top;
+
+if(top<window.innerHeight-100){
+
+card.style.opacity="1";
+
+card.style.transform="translateY(0)";
+
+}
+
+});
+
+});
